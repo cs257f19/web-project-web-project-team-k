9bc1c9588fe247a003376348b77102403633c3f2
yee-luna-c <yeec@carleton.edu>
2019-10-16 18:21:11 -0500

    added tabler for sql

---
+++ web/sql/createtable.sql
@@ -0,0 +1,23 @@
+DROP TABLE IF EXISTS executions;
+CREATE TABLE executions (
+  "Study Number" int,
+  "Edition Number" int,
+  "Part Number" int,
+  "Case Number" int,
+  "Race" int,
+  "Age at Execution" int,
+  "Place of Execution" int,
+  "Jurisdiction of Execution" int,
+  "Crime Comitted" int,
+  "Method of Execution" int,
+  "Day" int,
+  "Month" int,
+  "Year" int,
+  "Check Digit" int,
+  "State of Execution" int,
+  "County of Conviction" int,
+  "ICPSR State Code" int,
+  "Sex" int,
+  "Compensation Case" int,
+  "Occupation of Offender" int
+);
