12be15d5330f62d8d950ba836cff8db906542e6d
yee-luna-c <yeec@carleton.edu>
2019-10-24 16:57:31 -0500

    moved sql file to backend, changed format

---
+++ backend/sql/createtable.sql
@@ -0,0 +1,15 @@
+DROP TABLE IF EXISTS executions;
+CREATE TABLE executions (
+  race text,
+  age int,
+  place text,
+  jurisdiction text,
+  crime text,
+  method text,
+  day int,
+  month int,
+  year int,
+  state text,
+  county int,
+  sex text
+);
