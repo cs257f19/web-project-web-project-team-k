0bc3bfd84bc8a45b52cff124ebd3c33134ed2c88
yee-luna-c <yeec@carleton.edu>
2019-11-10 16:36:22 -0600

    created flask webapp, directories

---
+++ web/webapp.py
@@ -0,0 +1,28 @@
+from flask import Flask, request, render_template
+import json
+import sys
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/', methods=['POST'])
+def my_form_post():
+    text = request.form['text']
+    processed_text = text.upper()
+    return processed_text
+
+@app.route('/interactor')
+def interactor():
+    return render_template('interactor.html')
+
+if __name__ == '__main__':
+    if len(sys.argv) != 3:
+        print('Usage: {0} host port'.format(sys.argv[0]), file=sys.stderr)
+        exit()
+
+    host = sys.argv[1]
+    port = sys.argv[2]
+    app.run(host=host, port=port)
