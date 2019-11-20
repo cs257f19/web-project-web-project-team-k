237ca7514bc0a3e2c4e104a4194799526f9354d4
yee-luna-c <yeec@carleton.edu>
2019-11-20 12:08:54 -0600

    fixed select empty GET strings

--- web/static/scripts/main.js
+++ web/static/scripts/main.js
@@ -3,4 +3,7 @@ $('form').submit(function(e){
     var emptyinputs = $(this).find('input').filter(function(){
         return !$.trim(this.value).length;  // get all empty fields
     }).prop('disabled',true);
+      var emptyselects = $(this).find('select').filter(function(){
+          return !$.trim(this.value).length;  // get all empty fields
+      }).prop('disabled',true);
 });
